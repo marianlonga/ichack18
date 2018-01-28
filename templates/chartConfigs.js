1605582c0d2229bf76c741b5d917d8cf4cba3a24
El-Geresy <wbe15@ic.ac.uk>
2018-01-28 07:24:56 +0000

    Colours!
    Improve colours of the graphs

--- templates/chartConfigs.js
+++ templates/chartConfigs.js
@@ -5,25 +5,20 @@ var barChartConfig = {
         datasets: [{
             label: 'Consumption',
             data: [0, 0, 0, 0, 0], //We will put our data here
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+            backgroundColor: ['rgba(255, 99, 132, 0)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255, 99, 132, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 3
         },
         {
             label: '% Consumption',
             data: [0, 0, 0, 0, 0], //We will put our data here
+            backgroundColor: ['rgba(54, 162, 235, 0)']
+            ,
+            borderColor: ['rgba(54, 162, 235, 1)'],
+            borderWidth: 3
 
         }
       ]
@@ -96,39 +91,23 @@ var barChartConfig3 = {
             label: 'Consumption',
             data: [0, 0, 0, 0, 0], //We will put our data here
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(255, 99, 132, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(255,99,132,1)'
             ],
-            borderWidth: 1
+            borderWidth: 3
         },
         {
             label: '% Consumption',
             data: [0, 0, 0, 0, 0], //We will put our data here
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
+                'rgba(54, 162, 235, 0.2)'
             ],
             borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
+                'rgba(54, 162, 235, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 3
         }
       ]
     },
